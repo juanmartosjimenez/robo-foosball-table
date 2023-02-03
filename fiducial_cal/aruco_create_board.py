f1d866bbd7ca26a3d0f097bcfbaa5d209a046657
juan <19martj@gmail.com>
2023-02-03 15:41:33 -0500

    aruco marker creation and detection working

--- fiducial_cal/aruco_create_board.py
+++ fiducial_cal/aruco_create_board.py
@@ -1,10 +1,13 @@
+import time
+
 import cv2 as cv
 from numpy import size
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from camera.utilities import get_screenshot
 
 
-def generate_marker():
+def generate_markers():
     """
     Generate four aruco markers and save them to disk. One for each corner of the foosball table.
     :return:
@@ -16,18 +19,28 @@ def generate_marker():
 
 
 # aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_1000)
-def main():
+def detect_markers():
     aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
-    print(aruco_dict)
-    # Dimensions in cm
-    marker_length = 0.04
-    marker_separation = 0.01
     aruco_params = cv.aruco.DetectorParameters()
     detector = cv.aruco.ArucoDetector(aruco_dict, aruco_params)
-    board = cv.aruco.GridBoard(
-        size(5, 7), marker_length, marker_separation, aruco_dict)
-    cv.imwrite("Marker.png", board)
+
+    realsense_screenshot = get_screenshot()
+    gray = cv.cvtColor(realsense_screenshot, cv.COLOR_BGR2GRAY)
+
+    corners, ids, rejected = detector.detectMarkers(realsense_screenshot)
+    print(corners, ids, rejected)
+    # verify *at least* one ArUco marker was detected
+    if len(corners) > 0:
+        cv.aruco.drawDetectedMarkers(realsense_screenshot, corners, ids)
+        cv.imshow("Image", realsense_screenshot)
+        cv.waitKey(0)
+
+    if rejected:
+        cv.aruco.drawDetectedMarkers(realsense_screenshot, rejected, borderColor=(0, 0, 255))
+        cv.imshow("Image", realsense_screenshot)
+        cv.waitKey(0)
+
 
 
 if __name__ == '__main__':
-    generate_marker()
+    detect_markers()
