284320f285e770571e5c45528587dd084608b7ad
gusunny29 <53237770+gusunny29@users.noreply.github.com>
2023-04-07 19:25:43 -0400

    Linking Frontend and Backend

--- foosball_app/src/App.tsx
+++ foosball_app/src/App.tsx
@@ -1,116 +1,155 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import React from "react";
 import "./App.css";
-import "./tailwind.css";
 import image from "./assets/image.png";
 import axios from "axios";
 import WhiteCircle from "./WhiteCircle";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Card} from "react-bootstrap";
 
 
 
-
+interface Coordinates {
+  x: number;
+  y: number;
+}
 const App = () => {
   const [startPressed, setStartPressed] = useState(false);
+  const [powerOn, setPowerOn] = useState(false);
   const [resetPressed, setResetPressed] = useState(false);
-  const [stopPressed, setStopPressed] = useState(false);
-  const [coordinates, setCoordinates] = useState([0, 0]);
+  const [coordinates, setCoordinates] = useState<Coordinates>({x: 0, y: 0});
+  const [statusMessage, setStatusMessage] = useState('');
 
   const [circleX, setCircleX] = useState(0);
   const [circleY, setCircleY] = useState(0);
 
+  const handlePowerOn = async() => {
+    setPowerOn(true);
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/power_on', {
+        hardware_type: 'printer',
+        action: 'start',
+      });
+      setStatusMessage('Hardware started successfully');
+    } catch (error) {
+      setStatusMessage('Failed to start hardware');
+    }
+  };
+
   const handleStartPress = async() => {
     setStartPressed(true);
-    const response = await fetch('/start');
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/start', {
+        hardware_type: 'printer',
+        action: 'start',
+      });
+      setStatusMessage('Hardware started successfully');
+    } catch (error) {
+      setStatusMessage('Failed to start hardware');
+    }
+  };
     // Create function for starting the game 
-  }
 
   const handleResetPress = async() => {
     setResetPressed(true);
-    const response = await fetch('/reset');
-    const data = await response.json();
-    console.log(data);
-  }
-
-  const handleStopPress = async() => {
-    setStopPressed(true);
-    const response = await fetch('/stop');
-    const data = await response.json();
-    console.log(data);
-  }
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
-    const imageRect = event.currentTarget.getBoundingClientRect();
-    const imageWidth = 680;
-    const imageHeight = 330;
-    const x = event.clientX - imageRect.left - 10;
-    const y = event.clientY - imageRect.top - 10;
-
-    const adjusted_x = imageWidth - x;
-    const adjusted_y = imageHeight - y;
-
-    setCircleX(x);
-    setCircleY(y);
-    setCoordinates([adjusted_x, adjusted_y]);
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/reset', {
+        hardware_type: 'printer',
+        action: 'start',
+      });
+      setStatusMessage('Hardware started successfully');
+    } catch (error) {
+      setStatusMessage('Failed to start hardware');
+    }
   };
 
-  /*
+  const handleCircleMove = () => {
+    axios.get('http://127.0.0.1:5000/api/coordinates')
+      .then(response => setCoordinates(response.data))
+      .catch(error => console.error(error));
+    setCircleX(coordinates.x);
+    setCircleY(coordinates.y);
+  };
+
+  
   useEffect(() => {
-    axios.get('/coordinates')
-    .then(response => {
-      setCoordinates(response.data);
-    }).catch(error => {
-      console.error(error);
-    });
-  }, [coordinates]);
-  */
+    const intervalId = setInterval(() => {
+      axios.get('http://127.0.0.1:5000/api/coordinates')
+      .then(response => setCircleX(response.data.x))
+      .catch(error => console.error(error));
+    setCircleX(coordinates.x);
+    setCircleY(coordinates.y);
+    console.log(coordinates);
+    console.log(circleX);
+    console.log(circleY);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+  
 
   return (
-    <div className="robo-foosball-ui">
+    <div >
       <div className="rectangle-1">
         <div className = "right">
             <img className="image-1" src={image} alt="northeastern logo"/>
         </div>
 
-        <div className="video-text-box">
-            Robotic Foosball Table
-        </div>
-
-
+        <Card style = {{color: 'gray'}}>
+          <Card.Body style={{ fontSize: '50px' }}
+                      className= "text-center">
+            <Card.Title>RIFT</Card.Title>
+            <Card.Subtitle>Robotic Intelligence Foosball Table</Card.Subtitle>
+          </Card.Body>
+        </Card>
+      
         <div className="flex-container-1">
-          <button 
+          <Button 
+            size="lg"
+            style= {{fontSize: '20px'}}
+            className={powerOn ? 'dimmed' : 'not-pressed'}
+            onClick={handlePowerOn}
+          >
+            PowerOn 
+          </Button>
+
+          <Button
+            size="lg"
+            style= {{fontSize: '20px'}}
             className={startPressed ? 'dimmed' : 'not-pressed'}
             onClick={handleStartPress}
           >
             Start Game
-          </button>
-          <button 
+          </Button>
+          
+          <Button 
+            size="lg"
+            style= {{fontSize: '20px'}}
             className={resetPressed ? 'dimmed' : 'not-pressed'}
             onClick={handleResetPress}
           >
-            Reset Game 
-          </button>
-          <button 
-            className={stopPressed ? 'dimmed' : 'not-pressed'}
-            onClick={handleStopPress}
-          >
-            Emerg. Stop
-          </button>
+            Stop Game 
+          </Button>
         </div>
         
-        <div className="video-text-box">
-          Video Feed of Foosball Table
-        </div>
-        <div onMouseMove={handleMouseMove}>
+        <Card style = {{color: 'gray', marginBottom: '5px'}}>
+          <Card.Body style={{ fontSize: '24px' }}>
+            <Card.Title>Current Game State</Card.Title>
+          </Card.Body>
+        </Card>
+        <div>
           <WhiteCircle imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxMLxE4TIhrIVc-5M3Jiv7Z6R2ho8VtlZ_c0U4Q8BJ&s" circleX={circleX} circleY={circleY}/>
         </div>
          
-        <div className="video-text-box">
-          <span className="xy">
-            Coordinates of Ball: [ {coordinates[0]} , {coordinates[1]} ]
-          </span>
-        </div>
+        <Card style = {{color: 'gray', marginTop: '5px'}}>
+          <Card.Body style={{ fontSize: '24px' }}>
+            <Card.Title>Coordinates of Ball: [ {coordinates.x} , {coordinates.y} ]</Card.Title>
+          </Card.Body>
+        </Card>
+            
       </div>
     </div>
   );
